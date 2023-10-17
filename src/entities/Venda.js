d6d086745bc82c6cec3d27e3fc5efb0b14c34d15
Pedro Silva <pedrohrsilva2004@hotmail.com>
2023-10-17 13:32:19 -0300

    Classe Venda criada
    Signed-off-by: Pedro Silva <pedrohrsilva2004@hotmail.com>

---
+++ src/entities/Venda.js
@@ -0,0 +1,9 @@
+export default class Venda{
+    constructor(codVenda, codCliente, codFuncionario, dtVenda, horaVenda){
+        this.codVenda = codVenda,
+        this.codCliente = codCliente,
+        this.codFuncionario = codFuncionario,
+        this.dtVenda = dtVenda,
+        this.horaVenda = horaVenda
+    }
+}
