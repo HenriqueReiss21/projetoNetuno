e40cd738d5e655f0236acd523d72be7d9de7281b
ElanScarton <scartonelan@gmail.com>
2023-10-17 14:49:08 -0300

    Tabela funcionario

---
+++ src/entities/Funcionarios.js
@@ -0,0 +1,26 @@
+export default class Cliente{
+    constructor(codFuncionario, nome, cpf, rg, endereco, cep, cidade, estado, dataNascimento, dataCadastro, ativo, estadoCivil, numeroFilhos, pensão, função, setor, horario, salario, agencia, conta, cep, email){
+        this.codFuncionario = codFuncionario,
+        this.nome = nome,
+        this.cpf = cpf,
+        this.rg = rg
+        this.endereco = endereco,
+        this.cep = cep
+        this.cidade = cidade, 
+        this.estado = estado,
+        this.dataNascimento = dataNascimento,
+        this.Cadastro = dataCadastro,
+        this.ativo = ativo,
+        this.estadoCivil = estadoCivil,
+        this.numeroFilhos = numeroFilhos,
+        this.pensão = pensão,
+        this.função = função,
+        this.setor = setor,
+        this.horario = horario,
+        this.salario = salario,
+        this.agencia = agencia,
+        this.conta = conta,
+        this.cep = cep,
+        this.email = email
+    }
+}
