1937d846d9e55c1e11477c7af4db1e7f884f139e
Henrique Reis <henriquereiss2101@gmail.com>
2023-11-10 16:49:59 -0300

    Criacao ProdutoServicoModel + ProdutoServicoRoutes

--- src/entities/ProdutoServico.js
+++ src/entities/ProdutoServico.js
@@ -1,6 +1,6 @@
 export default class ProdutoServico{
-    constructor(codItem, servico, nome){
-        this.codItem = codItem,
+    constructor(id, servico, nome){
+        this.id = id,
         this.servico = servico,
         this.nome = nome
     }
